6b4c8ecc6e796880b735fbb14c323bf34173957a
Stan Schwertly <Stan@schwertly.com>
2014-10-17 14:42:28 -0400

    add redisPool module

---
+++ redisPool/redisPool.go
@@ -0,0 +1,30 @@
+package redisPool
+
+import (
+	"github.com/Stantheman/youtube-gif-go/config"
+	"github.com/garyburd/redigo/redis"
+	"time"
+)
+
+var Pool = pool()
+
+func pool() *redis.Pool {
+	conf := config.Get()
+	connect := conf.Redis.Ip + ":" + conf.Redis.Port
+
+	return &redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", connect)
+			if err != nil {
+				return nil, err
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
